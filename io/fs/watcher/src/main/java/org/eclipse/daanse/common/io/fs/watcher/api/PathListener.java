da3f1c5ad00f11bb8a1faaaa82498bd168b454b6
Stefan Bischof <stbischof@bipolis.org>
2024-01-30 16:17:13 +0100

    [io.watcher] minimal api
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ io/fs/watcher/src/main/java/org/eclipse/daanse/common/io/fs/watcher/api/PathListener.java
@@ -0,0 +1,59 @@
+/*
+* Copyright (c) 2024 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   SmartCity Jena - initial
+*   Stefan Bischof (bipolis.org) - initial
+*/
+package org.eclipse.daanse.common.io.fs.watcher.api;
+
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.util.List;
+
+import org.osgi.annotation.versioning.ConsumerType;
+
+/**
+ * The listener interface for receiving path events. The class that is
+ * interested in processing a path event implements this interface.
+ *
+ * @see PathEvent
+ */
+@ConsumerType
+public interface PathListener {
+
+    /**
+     * Handle base path.
+     *
+     * Base Paths are the root Directory that are observed. Would be called once
+     * before activating the Listener. Even when observing sub-directories, this
+     * method is called only once with the root directory.
+     *
+     * @param basePath the base path
+     */
+    void handleBasePath(Path basePath);
+
+    /**
+     * Handle initial paths.
+     *
+     * Initial Paths are all Paths that exist before the Listener is activated.
+     *
+     * @param paths the paths
+     */
+    void handleInitialPaths(List<Path> paths);
+
+    /**
+     * Handle path event.
+     *
+     * @param path the path
+     * @param kind the kind
+     */
+    void handlePathEvent(Path path, WatchEvent.Kind<Path> kind);
+
+}
